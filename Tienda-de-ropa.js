47e6619057b675165bf0c5252459cc6b1d8f3589
Timoteo Rodriguez <timoteop.rodriguez@gmail.com>
2023-09-04 21:08:25 -0300

    Termine la primer pre-entrega

---
+++ Tienda-de-ropa.js
@@ -0,0 +1,27 @@
+function TiendaDeRopa(){ 
+  let comando;
+    while (comando != "salir") {
+      comando = prompt("Tenes 3 opciones:\n Salir \n Ver productos \n Enviar peticiones")
+
+
+      switch (comando) {
+        case "salir":
+          alert("cerrando tienda de ropa...")
+          break;
+        default:
+          alert("Prenda no encontrada")
+        break;
+        case "Ver productos":
+          for (let remera = 1; remera <= 5; remera++)
+            alert("Este es la remera n° " + remera)
+          let remeras = prompt("Cual remera te gusto mas?")
+            alert("Gracias por compartir tu opinion") 
+          break;
+        case "Enviar peticiones":
+          let comentario = prompt("Que prenda te gustaria en la pagina?")
+           alert("Muchas Gracias, intentaremos que entren mas " + comentario)
+          }
+          break;}}
+
+
+TiendaDeRopa()

0bc89673728a6757e6d50dd8dcc219e915618152
Timoteo Rodriguez <timoteop.rodriguez@gmail.com>
2023-09-21 19:41:46 -0300

    termine la 3da Pre-Entrega

--- Tienda-de-ropa.js
+++ Tienda-de-ropa.js
@@ -1,25 +1,41 @@
-function TiendaDeRopa(){ 
-  let comando;
-    while (comando != "salir") {
-      comando = prompt("Tenes 3 opciones:\n Salir \n Ver productos \n Enviar peticiones")
+class Item{
+    constructor(nombre,precio,imagen){
+        this.nombre = nombre;
+        this.precio = precio;
+        this.imagen = imagen;
+    }
+}
+
+const remera = new Item("Remera alien", 60, "ropa og 1.PNG")
+const buzo = new Item("Buzo negro", 90, "Buzo og .PNG")
+const short = new Item("Short dorado", 200, "short epico.PNG")
+
+const billetera = [];
 
 
-      switch (comando) {
-        case "Salir":
-          alert("cerrando tienda de ropa...")
-          break;
-        default:
-          alert("Prenda no encontrada")
-        break;
-        case "Ver productos":
-          for (let remera = 1; remera <= 5; remera++)
-            alert("Este es la remera n° " + remera)
-          let remeras = prompt("Cual remera te gusto mas?")
-            alert("Gracias por compartir tu opinion") 
-          break;
-        case "Enviar peticiones":
-          let comentario = prompt("Que prenda te gustaria en la pagina?")
-           alert("Muchas Gracias, intentaremos que entren mas " + comentario)
-          }
-          break;}}
+let oro = 1000;
 
+const elOro = document.querySelector("#oro span");
+elOro.innerText = oro; 
+const laBilletera = document.getElementById("billetera");
+
+function comprar(Item){
+    if (oro - Item.precio >= 0){
+      billetera.push(Item);
+      oro -= Item.precio;
+      actualizarHTML();
+    }else{
+        alert(`No tenes los suficientes pesos para comprar ${Item.nombre}.`);
+    }
+}
+
+function actualizarHTML(){
+  elOro.innerText = oro;
+  elInventario.innerHTML = "";
+  for (const Item of billetera){
+    //const indice = billetera.indexOf(Item);
+    const li = `<li><img src="img/ ${Item.imagen} " alt=" ${Item.imagen} " /></li>`;
+    laBilletera.innerHTML += li;
+  }
+
+}
